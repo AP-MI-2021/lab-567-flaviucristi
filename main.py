ec785568063cad1f469903ab5bb086155d2b7f10
Muresan Flaviu <flaviucristi@yahoo.com>
2021-11-01 23:32:11 +0200

    Al cincelea commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from UI.console import runMenu
 def main():
     runAllTests()
     list=[]
-    list = adaugaObiect("1", "telefon", "iphone", 4000, "Cluj", list)
+    list = adaugaObiect("1", "telefon", "iphone", 5000, "Cluj", list)
     list = adaugaObiect("2", "carte", "programare", 50, "Arad", list)
     list = adaugaObiect("3", "carte", "desenat", 30, "Aiud", list)
     list = adaugaObiect("4", "telefon", "samsung", 3500, "Iasi", list)
